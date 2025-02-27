164500ce2785e58010493b74c5107c1da4943694
kwakyongwoo <kkolbuyw@gmail.com>
2025-02-27 18:57:13 +0900

    deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ sudo docker rmi kkolbuyw/jemyeonso-fastapi:latest
 
 sudo docker pull kkolbuyw/jemyeonso-fastapi:latest
 
-docker run -d -v $ENV_PATH:/fastapi/src/.env -p 8080:8000 --env-file .env --name app kkolbuyw/jemyeonso-fastapi:latest
+docker run -d -v $ENV_PATH:/fastapi/src/.env -p 8080:8000 --name app kkolbuyw/jemyeonso-fastapi:latest
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
