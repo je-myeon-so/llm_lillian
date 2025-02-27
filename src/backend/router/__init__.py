9e30ccba0a165babe25f246b0d9c0e2a363a3007
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-02-27 19:54:24 +0900

    add init

---
+++ src/backend/router/__init__.py
@@ -0,0 +1,6 @@
+from src.backend.router.db_question_routes import router as question_router
+from src.backend.router.db_analysis_routes import router as analysis_router
+from src.backend.router.db_user_routes import router as user_router
+
+# 필요할 경우 라우터를 한 번에 가져오기 위한 리스트
+routers = [question_router, analysis_router, user_router]
