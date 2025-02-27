6eb9aa6880c706702e884a81e9ad181bf2cad414
kwakyongwoo <kkolbuyw@gmail.com>
2025-02-28 04:09:44 +0900

    interview api

--- src/main.py
+++ src/main.py
@@ -22,16 +22,9 @@ app = FastAPI(
     version="1.0.0"
 )
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
