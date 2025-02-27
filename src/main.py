785436b8cf0f121ecb837a78cac13c163692589b
jungmin <121072020+jungmin827@users.noreply.github.com>
2025-02-27 11:53:37 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from router.analysis import router as api_router_analysis
+from router.question_routes import router as api_router_generate
 
 app = FastAPI(
     title="Interview Analysis API",
@@ -13,6 +14,7 @@ def read_root():
     return {"message": "Welcome to the Job Qualification Analysis API"}
 
 app.include_router(api_router_analysis, prefix="/api")
+app.include_router(api_router_generate, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
