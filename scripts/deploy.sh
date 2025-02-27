b08a703184a60b88155ede394013b52f2ddfe515
kwakyongwoo <43498544+kwakyongwoo@users.noreply.github.com>
2025-02-27 13:16:53 +0900

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
+
+sudo docker rmi kkolbuyw/jemyeonso-fastapi:latest
+
+sudo docker pull kkolbuyw/jemyeonso-fastapi:latest
+
+docker run -d -p 8080:8080 --name app kkolbuyw/jemyeonso-fastapi:latest
+
+docker rmi -f $(docker images -f "dangling=true" -q) || true
