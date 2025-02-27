28e60d42d7d71dcf5caefdf16a7265f8347cbdec
kwakyongwoo <43498544+kwakyongwoo@users.noreply.github.com>
2025-02-27 13:21:35 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ sudo docker rmi kkolbuyw/jemyeonso-fastapi:latest
 
 sudo docker pull kkolbuyw/jemyeonso-fastapi:latest
 
-docker run -d -p 8080:8080 --name app kkolbuyw/jemyeonso-fastapi:latest
+docker run -d -p 8080:8000 --name app kkolbuyw/jemyeonso-fastapi:latest
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
