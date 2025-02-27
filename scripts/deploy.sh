495dc8729deb186d24b3e95ef2660f62f46a10a4
kwakyongwoo <43498544+kwakyongwoo@users.noreply.github.com>
2025-02-27 18:23:12 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
+ENV_PATH="/home/ubuntu/env/.env"
+
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
 sudo docker rmi kkolbuyw/jemyeonso-fastapi:latest
 
 sudo docker pull kkolbuyw/jemyeonso-fastapi:latest
 
-docker run -d -p 8080:8000 --env-file .env --name app kkolbuyw/jemyeonso-fastapi:latest
+docker run -d -v $ENV_PATH:/fastapi/src/.env -p 8080:8000 --env-file .env --name app kkolbuyw/jemyeonso-fastapi:latest
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
