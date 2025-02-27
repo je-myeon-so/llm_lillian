490b7a589ce3b75cd6fd56dc1fc450d27f4df2af
kwakyongwoo <kkolbuyw@gmail.com>
2025-02-27 22:29:05 +0900

    RDS connect

--- src/backend/database_config.py
+++ src/backend/database_config.py
@@ -15,14 +15,14 @@ load_dotenv()
 # DATABASE = "jemyeonso"
 
 # 환경 변수에서 DB 정보 가져오기
-MYSQL_USER = os.getenv("MYSQL_USER", "root")  # 기본값 설정
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "ysy5282!")
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_USER = os.getenv("MYSQL_USER", "admin")  # 기본값 설정
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "l1HpWHZYwvWCBAQfILo9")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "jemyeonsodb.cbsa6mw24cwc.ap-northeast-2.rds.amazonaws.com")
 MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DB = os.getenv("MYSQL_DB", "test")
+MYSQL_DB = os.getenv("MYSQL_DB", "jemyeonso")
 
 # 데이터베이스 URL 설정
-DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+DATABASE_URL = f'mysql+pymysql://{MYSQL_USER}:{quote(MYSQL_PASSWORD)}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4'
 
 # 엔진 생성
 engine = create_engine(DATABASE_URL, echo=True)
