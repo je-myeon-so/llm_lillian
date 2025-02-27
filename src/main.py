a22ec0e0ef7e15dbaa699842a216b0e6c923761a
jungmin <121072020+jungmin827@users.noreply.github.com>
2025-02-27 11:46:35 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,13 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # 모든 도메인 허용
-    allow_credentials=True,
-    allow_methods=["*"],  # 모든 HTTP 메서드 허용
-    allow_headers=["*"],  # 모든 헤더 허용
-)
+
 
 app.include_router(question_routes.router)
 
